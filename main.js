0a960809f0ac9d98562dfd04a34a670eb2573dc0
hoschi-it <hoschi-it@web.de>
2019-09-06 22:16:50 +0200

    finishing input of mathematical term

---
+++ main.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        input: "",
+    },
+    computed: {
+        buttons: function(){
+            var btns = ["+", "-", "*", "/"];
+            for(var i = 0; i < 10; i++){
+                btns += i;
+            }
+            return btns;
+        },
+        calculation: function(){
+            var calc = {"a": null, "operator": null, "b": null};
+            var regex = /([0-9\.]*)([\+\-\*\/]{1})([0-9\.]*)/
+            var match = this.input.match(regex);
+            if(match){
+                calc.a = parseFloat( match[1]);
+                calc.operator = match[2];
+                calc.b = parseFloat(match[3]);
+            }
+            return calc;
+        },
+        solution: function(){
+            var c = this.calculation;
+            if(c.a && c.operator && c.b){
+                switch(c.operator){
+                    case "+": return c.a + c.b; break;
+                    case "-": return c.a . c.b; break;
+                    case "*": return c.a * c.b; break;
+                    case "/": return c.a / c.b; break;
+                }
+            }
+        }
+    },
+    methods: {
+        write: function(text){
+            this.input += text;
+        },
+    }
+});
+
